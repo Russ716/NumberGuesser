7aa59267c38e1ae992e77a128f0d83dff461ce72
Russ <iamkrashenbern@yahoo.com>
2023-04-13 11:23:48 -0500

    Phase 6

--- Program.cs
+++ Program.cs
@@ -1,29 +1,51 @@
 ﻿using System;
 
-class Program
+namespace GuessTheNumber
 {
-    static void Main(string[] args)
+    class Program
     {
-        int secretNumber = 42;
-        int maxGuesses = 4;
-        for (int i = 1; i <= maxGuesses; i++)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Guess the secret number! You have " + (maxGuesses - i + 1) + " guesses remaining.");
-            Console.Write($"Enter guess #{i}: ");
-            int guess = int.Parse(Console.ReadLine());
-            if (guess == secretNumber)
+            // Generate a random number between 1 and 100
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            int guess;
+            int guessesLeft = 4;
+
+            // Loop until the user guesses correctly or runs out of guesses
+            while (guessesLeft > 0)
             {
-                Console.WriteLine("Congratulations, you guessed the secret number!");
-                return;
+                Console.WriteLine("Guess the secret number (between 1 and 100). You have {0} guesses left.", guessesLeft);
+
+                // Read the user's guess as input
+                guess = int.Parse(Console.ReadLine());
+
+                // Check if the guess is correct
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the secret number!");
+                    return;
+                }
+                else
+                {
+                    // Tell the user if their guess was too high or too low
+                    if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Your guess was too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess was too low.");
+                    }
+
+                    guessesLeft--;
+                }
             }
-            else if (i == maxGuesses)
-            {
-                Console.WriteLine("Sorry, you are out of guesses. The secret number was: " + secretNumber);
-            }
-            else
-            {
-                Console.WriteLine("Sorry, your guess is incorrect. Please try again.");
-            }
+
+            // If the user runs out of guesses, display the secret number
+            Console.WriteLine("Sorry, you ran out of guesses. The secret number was {0}.", secretNumber);
         }
     }
 }
+
