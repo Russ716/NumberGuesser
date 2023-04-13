29dc7711fd020122b3124e15ca25348d4078b51c
Russ <iamkrashenbern@yahoo.com>
2023-04-13 10:57:54 -0500

    Phase 3

--- Program.cs
+++ Program.cs
@@ -5,18 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Guess the secret number!");
-        Console.Write("Enter your guess: ");
-        string guess = Console.ReadLine();
-        int parsedGuess = int.Parse(guess);
-        // Console.WriteLine("Your guess was: " + guess);
-        int num = 42;
-        if (num == parsedGuess)
+        for (int i = 0; 1 < 4; i++)
         {
-            Console.WriteLine("You got it!");
-        }
-        else
-        {
-            Console.WriteLine("Guess again.");
+
+            Console.Write("Enter your guess: ");
+            string guess = Console.ReadLine();
+            int parsedGuess = int.Parse(guess);
+            // Console.WriteLine("Your guess was: " + guess);
+            int num = 42;
+            if (num == parsedGuess)
+            {
+                Console.WriteLine("You got it!");
+            }
+            else
+            {
+                Console.WriteLine("Guess again.");
+            }
+
         }
     }
 }
